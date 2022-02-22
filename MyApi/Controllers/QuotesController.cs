b87534f772adca7b7f8fda792c93c4854a8bf569
Tech Wiz <enoch.mwangi@outlook.com>
2022-02-22 16:16:08 +0300

    Add project files.

---
+++ MyApi/Controllers/QuotesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyApi.Models;
+using System.Collections.Generic;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        static List<Quote> _quotes = new List<Quote>()
+        {
+            new Quote(){Id = 0, Title = "Titel 1", Author = "John Doe", Desription = "Description 1"},
+            new Quote(){Id = 1, Title = "Title 2", Author = "Author 2", Desription = "Desciption 2"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Quote quote)
+        {
+            _quotes.Add(quote);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Quote quote)
+        {
+            _quotes[id] = quote;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _quotes.RemoveAt(id);
+        }
+    }
+}
