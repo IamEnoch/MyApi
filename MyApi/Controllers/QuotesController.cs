3e51a8e05a6cc9ef70d19ebf4a1ae73f0fe7a9f4
Tech Wiz <enoch.mwangi@outlook.com>
2022-02-22 19:33:21 +0300

    Changed CRUD handling to use dbcontext that was created

--- MyApi/Controllers/QuotesController.cs
+++ MyApi/Controllers/QuotesController.cs
@@ -17,29 +17,44 @@ namespace MyApi.Controllers
             _quotesDbContext = quotesDbContext;
         }
 
-
+        //Get all quotes
         [HttpGet]
         public IEnumerable<Quote> Get()
         {
             return _quotesDbContext.Quotes;
         }
 
+        //Get: a particular quote
+        [HttpGet("{id}")]
+        public Quote Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            return quote;
+        }
+
         [HttpPost]
         public void Post([FromBody]Quote quote)
         {
-            _quotes.Add(quote);
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
         }
 
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]Quote quote)
         {
-            _quotes[id] = quote;
+            var entity = _quotesDbContext.Quotes.Find(id);
+            entity.Title = quote.Title;
+            entity.Author = quote.Author;
+            entity.Desription = quote.Desription;
+            _quotesDbContext.SaveChanges();
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            _quotes.RemoveAt(id);
+            var entity = _quotesDbContext.Quotes.Find(id);
+            _quotesDbContext.Quotes.Remove(entity);
+            _quotesDbContext.SaveChanges();
         }
     }
 }
